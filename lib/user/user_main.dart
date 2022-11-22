3ad28697449d76c894362b9a80f61a16b4927a8c
vishal2605 <77671133+vishal2605@users.noreply.github.com>
2022-11-22 10:30:55 +0530

    first commit

---
+++ lib/user/user_main.dart
@@ -0,0 +1,74 @@
+import 'package:assignment/user/profile.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/login.dart';
+import 'change_password.dart';
+import 'dashboard.dart';
+
+class UserMain extends StatefulWidget {
+  UserMain({Key? key}) : super(key: key);
+
+  @override
+  _UserMainState createState() => _UserMainState();
+}
+
+class _UserMainState extends State<UserMain> {
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[
+    Dashboard(),
+    Profile(),
+    ChangePassword(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Welcome User"),
+            ElevatedButton(
+              onPressed: () async => {
+
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                        (route) => false)
+              },
+              child: Text('Logout'),
+              style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+            )
+          ],
+        ),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'My Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Change Password',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
